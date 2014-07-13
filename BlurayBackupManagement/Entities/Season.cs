962f46e4c2b2f371ded8d91712fe44be87b71eb6
npatsiouras@gmail.com <npatsiouras@gmail.com>
2014-07-13 07:08:27 +0300

    Updated master to contain the library project

---
+++ BlurayBackupManagement/Entities/Season.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlurayBackupManagement.Entities
+{
+    public class Season
+    {
+        #region Properties
+        private int _number;
+        public int Number
+        {
+            get { return _number; }
+            private set { _number = value; }
+        }
+
+        public List<Episode> episodes;
+
+        public int EpisodeCount
+        {
+            get { return episodes.Count; }
+        }
+        #endregion
+
+        #region Inteface Properties
+
+        #endregion
+
+        #region Methods
+        public Season(int number)
+        {
+            _number = number;
+            episodes = new List<Episode>();
+        }
+
+        public bool IsWatched()
+        {
+            return (from eps in episodes where !eps.Watched select eps).ToList<Episode>().Count == 0;
+        }
+
+        public void AddEpisode(Episode episode)
+        {
+            //If it's not in the episode list
+            if((from eps in episodes where eps.Number == episode.Number select eps).ToList<Episode>().Count == 0){
+                //Add it
+                episodes.Add(episode);
+            }
+        }
+
+        public void AddEpisodes(params Episode[] episodesList)
+        {
+            foreach (Episode episode in episodesList)
+            {
+                AddEpisode(episode);
+            }
+        }
+        #endregion
+
+        #region Interface Methods
+
+        #endregion
+    }
+}
---
+++ BlurayBackupManagement/Managers/TvSeriesManager.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlurayBackupManagement.Entities;
+
+namespace BlurayBackupManagement.Managers
+{
+    public class TvSeriesManager : Singleton<TvSeriesManager>
+    {
+        #region Properties
+        Dictionary<char, List<TvSeries>> shows;
+        #endregion
+
+        #region Inteface Properties
+
+        #endregion
+
+        #region Event Handlers
+        public void OnNewDisk(BluerayDisk disk)
+        {
+            AddShows(disk.tvSeriesContents.ToArray());
+        }
+        #endregion
+
+        #region Methods
+        public TvSeriesManager()
+        {
+            shows = new Dictionary<char, List<TvSeries>>();
+            DiskManager.Instance.diskAdded += this.OnNewDisk;
+        }
+
+        public void AddShow(TvSeries show)
+        {
+            if (!shows.Keys.Contains(show.Name[0]))
+            {
+                shows.Add(show.Name[0], new List<TvSeries>());
+            }
+            if ((from s in shows[show.Name[0]] where s.Name == show.Name select s).ToList<TvSeries>().Count == 0)
+            {
+                char initial = show.Name[0];
+                if (shows[initial] == null)
+                {
+                    shows[initial] = new List<TvSeries>();
+                }
+                shows[initial].Add(show);
+            }
+        }
+
+        public void AddShows(params TvSeries[] showsList)
+        {
+            foreach (TvSeries show in showsList)
+            {
+                AddShow(show);
+            }
+        }
+
+        public void OnApplicationExit(object sender, EventArgs e)
+        {
+            _ApplicationQuitting = true;
+            shows.Clear();
+        }
+        #endregion
+
+        #region Interface Methods
+
+        #endregion
+    }
+}
